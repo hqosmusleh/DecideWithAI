12d8fb737377c255ec14e523243a08c1ac8f1ac8
hqosmusleh <hqosmusleh@gmail.com>
2025-09-29 10:38:36 -0400

    Update main.py

--- main.py
+++ main.py
@@ -120,3 +120,6 @@ Guidelines:
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
+@app.route("/healthz")
+def healthz():
+    return "OK", 200
